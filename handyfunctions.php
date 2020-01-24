a9e94885c3820eb88ad8fcdaea4a6cc920cefc9d
mutkajoa <mutkajoakim@gmail.com>
2020-01-24 10:43:48 +0200

    Added Handy functions

---
+++ handyfunctions.php
@@ -0,0 +1,24 @@
+<?php
+
+
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+  }
+function create_conn(){
+$servername = "localhost";
+$username = "mutkajoa";
+$password ="DM8J2ZFGnG";
+$dbname ="mutkajoa";
+$conn = new mysqli($servername,$username,$password,$dbname);
+$conn->set_charset('utf8');
+      if($conn->connect_error){
+        die("<p>CONNECTION FAILED: ".$conn->connect_error."</p>");
+      }
+      else{ 
+        return $conn;
+      };
+    }
+?>
