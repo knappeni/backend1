be067f82ebca146abcec0b3195e769425469998d
mutkajoakim <33824586+mutkajoa@users.noreply.github.com>
2020-01-27 09:43:04 +0200

    Reflektioner added

---
+++ reflektioner.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Reflektioner</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <h1>Reflektioner och feedback</h1>
+    <?php include 'navbar.php'?>
+
+<Section>
+<h3>Reflektioner</h3>
+<p>Jocke: Det är nu tredje gången jag går den här kursen, förhoppningsvis den sista också. Projekt verkar vara lika som i fjol, vilket underlättade<br>
+arbetet för oss i år avsevärt. Jag ser inget fel på att kursen är lika som i fjol eftersom jag tyckte den var bra i fjol också, jag blev<br>
+bara lat på slutrakan.</p>
+<p><ol>
+    <li>Du har en bra förmåga att förklara saker så att man förstår.</li>
+    <li>Dina exempel, att du går stegvis från att de inte fungerar, till att de fungerar lite, till att det fungerar fullt ut.</li>
+    <li>Att man har möjligheten att kolla på lektionerna i efterhand.</li>
+    </ol></p>
+<h2>Dåliga Saker</h2>
+<p><ol>
+    <li>Jag skulle personligen vilja ha en aningen snabbare pace på lektionerna.</li>
+    <li></li>
+    <li></li>
+    </ol></p>
+<h2>Lätt/Svår</h2>
+<p>Projekt 1 var ganska enkelt, men det kanske mest beror på att det är lika som i fjol och vi hade ganska mycke färdigt från fjolårets projekt.</p>
+<p></p>
+</Section>
+<?php
+
+   
+?>
+</body>
+</html>
